14d523a331c0cc00bebcb858afadba38121b488e
heraldOfMechanus <jccallahan11@gmail.com>
2021-08-17 16:35:43 -0500

    Added the HealthCheckServlet.

---
+++ src/main/java/com/revature/johnKimAPI/web/servlet/HealthCheckServlet.java
@@ -0,0 +1,16 @@
+package com.revature.johnKimAPI.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HealthCheckServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().write("{ \"status\": \"UP\" }");
+    }
+
+}
--- src/main/java/com/revature/johnKimAPI/web/util/ContextLoaderListener.java
+++ src/main/java/com/revature/johnKimAPI/web/util/ContextLoaderListener.java
@@ -8,6 +8,7 @@ import com.mongodb.client.MongoClient;
 import com.revature.johnKimAPI.repositories.SchoolRepository;
 import com.revature.johnKimAPI.service.ValidationService;
 import com.revature.johnKimAPI.util.GetMongoClient;
+import com.revature.johnKimAPI.web.servlet.HealthCheckServlet;
 import com.revature.johnKimAPI.web.servlet.TestServlet;
 import org.slf4j.LoggerFactory;
 import javax.servlet.ServletContext;
@@ -28,11 +29,13 @@ public class ContextLoaderListener implements ServletContextListener {
 
         // TODO: Add all of your servlets to here!
         TestServlet testServlet = new TestServlet();
+        HealthCheckServlet health = new HealthCheckServlet();
 //        UserServlet userServlet = new UserServlet(userService, mapper);
 //        AuthServlet authServlet = new AuthServlet(userService, mapper);
 //
         ServletContext servletContext = sce.getServletContext();
         servletContext.addServlet("TestServlet", testServlet).addMapping("/test");
+        servletContext.addServlet("HealthCheckServlet", health).addMapping("/health");
 //        servletContext.addServlet("UserServlet", userServlet).addMapping("/users/*");
 //        servletContext.addServlet("AuthServlet", authServlet).addMapping("/auth");
 
