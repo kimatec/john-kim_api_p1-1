3fa2c1643ae020a167e7572447da168a8f9c1f90
heraldOfMechanus <jccallahan11@gmail.com>
2021-08-23 14:24:56 -0500

    Removed unnecessary comment.

--- src/main/java/com/revature/johnKimAPI/web/filters/CorsFilter.java
+++ src/main/java/com/revature/johnKimAPI/web/filters/CorsFilter.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 @WebFilter("/*")
 public class CorsFilter extends HttpFilter {
 
-    // This is weird
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse resp, FilterChain chain) throws IOException, ServletException {
         req.setAttribute("filtered", true);
