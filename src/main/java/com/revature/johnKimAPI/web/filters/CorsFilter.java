da194d9eb73f3ae68a04d093d8add9460a8ba4d7
heraldOfMechanus <jccallahan11@gmail.com>
2021-08-24 16:46:05 -0500

    Switching CORS filter for testing.

--- src/main/java/com/revature/johnKimAPI/web/filters/CorsFilter.java
+++ src/main/java/com/revature/johnKimAPI/web/filters/CorsFilter.java
@@ -14,7 +14,8 @@ public class CorsFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse resp, FilterChain chain) throws IOException, ServletException {
         req.setAttribute("filtered", true);
-        resp.setHeader("Access-Control-Allow-Origin", "http://john-callahan-project-1.s3-website-us-east-1.amazonaws.com");
+//        resp.setHeader("Access-Control-Allow-Origin", "http://john-callahan-project-1.s3-website-us-east-1.amazonaws.com");
+        resp.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
         resp.setHeader("Access-Control-Allow-Credentials", "true");
         resp.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
         resp.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
